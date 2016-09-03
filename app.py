8a08106270f0325a8183eb530c1d12693b06da39
adarshmammen <adarsh.mammen@gmail.com>
2016-09-03 17:14:27 +0000

    added pandas frame

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import requests
 import datetime
+import pandas as pd
 
+dates = []
 now = datetime.datetime.now()
 last_month =  int(str(now).split('-')[1])-1
 
@@ -13,14 +15,17 @@ session = requests.Session()
 session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
 raw_data = session.get(api_url)
 
-for i,ele in enumerate(raw_data.json()["dataset"]['data']):
+
 	if int(str(ele[0]).split('-')[1]) == last_month:
 		close_prices_list.append(ele[4]) # field for closing price
+		dates.append(str(ele[0]))
 	if int(str(ele[0]).split('-')[1])< last_month:
 		break
 
 close_prices_list = close_prices_list[::-1]
-print close_prices_list
+pd_dates = pd.to_datetime(dates)
+
+df = pd.DataFrame(close_prices_list, index = pd_dates)
 
 app = Flask(__name__)
 
@@ -30,7 +35,7 @@ def main():
 
 @app.route('/index')
 def index():
-  return str(close_prices_list)
+  return str(str(df))
   return render_template('index.html')
 
 if __name__ == '__main__':
